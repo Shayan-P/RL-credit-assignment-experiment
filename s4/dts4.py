48c8de2718d5566b7689d24c5efc0cb4d84276e9
Kaustubh Dighe <kaustubhdighe1@gmail.com>
2024-05-10 05:41:09 -0400

    added DecisionS4, tried testing with DoorKey but DoorKeyDataset has bugs

---
+++ s4/dts4.py
@@ -0,0 +1,72 @@
+from .s4 import S4Block
+import torch
+import torch.nn as nn
+
+
+class DecisionS4(nn.Module):
+    def __init__(self, state_dim, act_dim, h_dim, context_len, drop_p, max_timestep=4096):
+        super().__init__()
+
+        self.state_dim = state_dim
+        self.act_dim = act_dim
+        self.h_dim = h_dim
+
+        ### transformer blocks
+        self.s4 = S4Block(d_model=h_dim, dropout=drop_p)
+
+        ### projection heads (project to embedding)
+        self.embed_ln = nn.LayerNorm(h_dim)
+        self.embed_timestep = nn.Embedding(max_timestep, h_dim)
+        self.embed_rtg = torch.nn.Linear(1, h_dim)
+        self.embed_state = torch.nn.Linear(state_dim, h_dim)
+        
+        # # discrete actions
+        self.embed_action = torch.nn.Embedding(act_dim, h_dim)
+        use_action_tanh = False # False for discrete actions
+
+        # continuous actions
+        # self.embed_action = torch.nn.Linear(act_dim, h_dim)
+        # use_action_tanh = True # True for continuous actions
+        
+        ### prediction heads
+        self.predict_rtg = torch.nn.Linear(h_dim, 1)
+        self.predict_state = torch.nn.Linear(h_dim, state_dim)
+        self.predict_action = nn.Sequential(
+            *([nn.Linear(h_dim, act_dim)] + ([nn.Tanh()] if use_action_tanh else []))
+        )
+
+
+    def forward(self, timesteps, states, actions, returns_to_go):
+
+        B, T, _ = states.shape
+
+        time_embeddings = self.embed_timestep(timesteps)
+
+        # time embeddings are treated similar to positional embeddings
+        state_embeddings = self.embed_state(states) + time_embeddings
+        action_embeddings = self.embed_action(actions) + time_embeddings
+        returns_embeddings = self.embed_rtg(returns_to_go) + time_embeddings
+
+        # stack rtg, states and actions and reshape sequence as
+        # (r1, s1, a1, r2, s2, a2 ...)
+        h = torch.stack(
+            (returns_embeddings, state_embeddings, action_embeddings), dim=1
+        ).permute(0, 2, 1, 3).reshape(B, 3 * T, self.h_dim)
+
+        h = self.embed_ln(h)
+        
+        # transformer and prediction
+        h = self.s4(h)
+
+        # get h reshaped such that its size = (B x 3 x T x h_dim) and
+        # h[:, 0, t] is conditioned on r_0, s_0, a_0 ... r_t
+        # h[:, 1, t] is conditioned on r_0, s_0, a_0 ... r_t, s_t
+        # h[:, 2, t] is conditioned on r_0, s_0, a_0 ... r_t, s_t, a_t
+        h = h.reshape(B, T, 3, self.h_dim).permute(0, 2, 1, 3)
+
+        # get predictions
+        return_preds = self.predict_rtg(h[:,2])     # predict next rtg given r, s, a
+        state_preds = self.predict_state(h[:,2])    # predict next state given r, s, a
+        action_preds = self.predict_action(h[:,1])  # predict action given r, s
+    
+        return state_preds, action_preds, return_preds
